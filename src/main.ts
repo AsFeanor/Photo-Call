3c6e3da071b2ad0351adb374d33d1bbeb03e2d97
Altuğ Ege Sarı <altugegesari@gmail.com>
2022-12-21 13:13:59 +0300

    enables cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:63342', 'http://localhost:63342/photo-call-ui/index.html?_ij_reload'],
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
